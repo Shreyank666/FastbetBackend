9b9657625a06fbf176deef574b9c0fc11d92c07f
Shreyank666 <101159535+Shreyank666@users.noreply.github.com>
2025-03-05 01:41:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,14 +39,14 @@ const io = socketIo(server, {
 port = 4000
 // CORS configuration
 
-// app.use(
-//   cors({
-//     origin: ["https://www.98fastbet.com", "https://admin.98fastbet.com"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
-//     methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
-//     credentials: true, // Allow credentials like cookies to be sent
-//   })
-// );
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://www.98fastbet.com", "https://admin.98fastbet.com"], // Replace '*' with the specific origin(s) you want to allow, e.g., 'https://yourdomain.com'
+    methods: ['POST', 'GET', 'PUT', 'DELETE'], // Define allowed HTTP methods
+    credentials: true, // Allow credentials like cookies to be sent
+  })
+);
+// app.use(cors());
 
 const MONGO_URI = process.env.mongodb_url;
 // MongoDB connection
