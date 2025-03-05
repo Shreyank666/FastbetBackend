1ef4af0bbd2d5e1dad8512cdb66bdc8fdc6fe9ef
Shreyank666 <101159535+Shreyank666@users.noreply.github.com>
2025-03-06 03:39:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -384,8 +384,7 @@ const fetchOdds = async () => {
         liveData.odds[marketId] = {
           matchName,  // ✅ Store match name
           matchOdds: response.data.result.team_data || [],  // ✅ Match Odds (Lagai/Khai)
-          fancyMarkets: response.data.result.session || [],
-          commissionFancy: response.data.result.commission_fancy_data || []
+          fancyMarkets: response.data.result.session || []
         };
       }
     }
